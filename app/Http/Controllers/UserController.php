6e4d2119bca19abfcd5befa0988600d6d45c5e0b
shofwahknk <shofwahknk@gmail.com>
2024-04-18 23:43:18 +0700

    Jobsheet-4_Practicum-1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -28,15 +28,38 @@ class UserController extends Controller
         // return view('user', ['data' => $user]);
 
         //tambah data user dengan Eloquent Model 
+        // $data = [
+        //     'nama' => 'Pelanggan Pertama',
+        // ];
+
+        // UserModel::where('username', 'customer-1')->update($data); // update data ke tabel m_user
+
+        //  //coba akses model UserModel
+        //  $user = UserModel::all(); // ambil semua data ke tabel m_user
+        //  return view('user', ['data' => $user]);
+
+        // JOBSHEET 4 
+
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_dua',
+        //     'nama' => 'Manager 2',
+        //     'password' => Hash::make('12345')
+        // ];
+        // UserModel::create($data); 
+
+        // $user = UserModel::all(); 
+        // return view('user', ['data' => $user]);\
+
         $data = [
-            'nama' => 'Pelanggan Pertama',
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345')
         ];
+        UserModel::create($data); 
 
-        UserModel::where('username', 'customer-1')->update($data); // update data ke tabel m_user
-
-         //coba akses model UserModel
-         $user = UserModel::all(); // ambil semua data ke tabel m_user
-         return view('user', ['data' => $user]);
-
+        $user = UserModel::all(); 
+        return view('user', ['data' => $user]);
     }
 }
