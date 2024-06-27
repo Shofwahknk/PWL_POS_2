1ae7f4d8492b08dc78f33fe98a2d76dadd9b4af7
Shofwah Kanaka <Shofwahknk@gmail.com>
2024-06-28 03:20:12 +0700

    Jobsheet-11_Practicum-1

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -8,26 +8,43 @@ use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-class UserModel extends Authenticatable implements JWTSubject
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
-class UserModel extends Model
-{
+class UserModel extends Authenticatable implements JWTSubject {
     use HasFactory;
 
     protected $table = 'm_user'; // Mendefinisikan nama tabel yang digunakan oleh model ini
     protected $primaryKey = 'user_id'; // Mendefinisikan primary key dari tabel yang digunakan
 
-    public function getJMTIdentifier(){
-        return $this ->getKey();
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
     }
-    public function getJWTCustomClaims(){
+
+    public function getJWTCustomClaims()
+    {
         return [];
     }
+    protected $table = 'm_users';        // Mendefinisikan nama tabel yang digunakan untuk model ini
+    protected $primaryKey = 'user_id';  // Mendefinisikan primary key dari tabel yang digunakan
+    protected $fillable = ['user_id', 'level_id', 'username', 'nama', 'password','image'];
+    // protected $fillable = ['level_id', 'username', 'nama'];
+
+    public function level(): BelongsTo
+    {
+        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($image) => url('/storage/posts/' . $image),
+        );
+    }
+}
     /**
      * The attributes that are mass assignable.
      * 
      * @var array
      */
     // protected $fillable = ['level_id','username','nama','password'];
-    protected $fillable = ['level_id','username','nama','password'];
-}
+ 
